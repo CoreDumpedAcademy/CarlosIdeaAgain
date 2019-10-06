87b14a93f119ce98f81127d7817e65af1cb426d9
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-10-06 08:20:08 +0200

    eliminated popover function in taller menu

--- redCore/src/app/talleres/talleres.page.ts
+++ redCore/src/app/talleres/talleres.page.ts
@@ -34,17 +34,19 @@ export class TalleresPage implements OnInit {
 
   public devWidth = (this.platform.width() > 992);
 
+  /*
   async showPopover(ad){
-    /*
+    
     const popover = await this.popoverCtr.create({      //Buscar solocion compatible con
       component: PopinfoComponent,                      //los foros para popovers
       componentProps: ad
     });
 
     await popover.present();
-    */
+    
   }
-
+  */
+ 
   public clicktest(event: any) {
     console.log("himom");
   }
